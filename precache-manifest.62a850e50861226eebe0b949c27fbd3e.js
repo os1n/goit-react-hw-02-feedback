0c3523885dd1fc12ce49302ebbb14a1246e53fe4
Dmytro Osin <mr.oda.ua@gmail.com>
2020-09-20 00:50:13 +0300

    Updates

---
+++ precache-manifest.62a850e50861226eebe0b949c27fbd3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b3389931a9d113bc8e1e91fbce9d53",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "5d04cde72251d11100b0",
+    "url": "/goit-react-hw-02-feedback/static/css/main.54c16b82.chunk.css"
+  },
+  {
+    "revision": "5ba446f1b5a37fdc6c80",
+    "url": "/goit-react-hw-02-feedback/static/js/2.236786e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.236786e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d04cde72251d11100b0",
+    "url": "/goit-react-hw-02-feedback/static/js/main.99a93f37.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
